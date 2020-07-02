4a2b566f7665662932a6ba91c49adfbac8d448c3
Lorenzo Prestes Machado <66543667+Hacker-Turbo@users.noreply.github.com>
2020-07-02 00:46:54 -0300

    Add files via upload

---
+++ MLGBot.py
@@ -0,0 +1,73 @@
+# Programado por MLG History Me adere no ifunny é MLGHistory, ou se inscreve no meu canal. MLG History, e me segue no twitter @arctroopper
+#
+import random
+logo = '''
+⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄
+⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄
+⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄
+⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰
+⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤  ┏━┓┏━┳┓  ┏━━━┳━━┓   ┏┓      ┏┓ ┏━━━┓
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗  ┃┃┗┛┃┃┃  ┃┏━┓┃┏┓┃  ┏┛┗┓    ┏┛┃ ┃┏━┓┃
+⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄  ┃┏┓┏┓┃┃  ┃┃ ┗┫┗┛┗┳━┻┓┏┛    ┗┓┃ ┃┃┃┃┃
+⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄  ┃┃┃┃┃┃┃ ┏┫┃┏━┫┏━┓┃┏┓┃┃      ┃┃ ┃┃┃┃┃
+⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄  ┃┃┃┃┃┃┗━┛┃┗┻━┃┗━┛┃┗┛┃┗┓    ┏┛┗┳┫┗━┛┃
+⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄  ┗┛┗┛┗┻━━━┻━━━┻━━━┻━━┻━┛    ┗━━┻┻━━━┛
+⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄  Programado por MLG History com a colaboração do TomToma
+⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄  Youtube: MLG History
+⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴  Twitter: @arctroopper, @TOMTOMA_iFunny
+⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠  iFunny: MLGHistory, TOMTOMA
+'''
+print (logo)
+print ('qual categoria de hentai vc quer?')
+print ('categorias disponíveis: hetero, trap e furry')
+categoria = input ()
+if categoria  == "furry":
+   furrys=[' https://nhentai.net/g/211170/ ' ,' https://nhentai.net/g/210910/ ',' https://nhentai.net/g/212653/ ',' https://nhentai.net/g/212457/ ','https://nhentai.net/g/211136/','https://nhentai.net/g/210590/','https://nhentai.net/g/210589/','https://nhentai.net/g/210275/','https://nhentai.net/g/207520/','https://nhentai.net/g/207444/','https://nhentai.net/g/207303/','https://nhentai.net/g/207271/','https://nhentai.net/g/207300/','https://nhentai.net/g/207151/','https://nhentai.net/g/207125/','https://nhentai.net/g/207101/','https://nhentai.net/g/207012/','https://nhentai.net/g/207011/','https://nhentai.net/g/207009/','https://nhentai.net/g/206970/','https://nhentai.net/g/206967/','https://nhentai.net/g/206668/','https://nhentai.net/g/206569/','https://nhentai.net/g/206381/','https://nhentai.net/g/206366/','https://nhentai.net/g/206301/','https://nhentai.net/g/206271/','https://nhentai.net/g/138509/','https://nhentai.net/g/136224/']
+   f=random.choice(furrys)
+   print (f)
+if categoria == "trap":
+    traps = ['https://nhentai.net/g/172937/', 'https://nhentai.net/g/259790/',
+            'https://nhentai.net/g/150023/','https://nhentai.net/g/211129/','https://nhentai.net/g/312919/','https://nhentai.net/g/283818/',
+            'https://nhentai.net/g/236195/','https://nhentai.net/g/107296/','https://nhentai.net/g/164361/','https://nhentai.net/g/263087/',
+            'https://nhentai.net/g/208445/']
+    t = random.choice(traps)
+    print(t)
+if categoria == "hetero":
+    hetero = ['https://nhentai.net/g/210910/18/','https://nhentai.net/g/243845/','https://nhentai.net/g/89379/','https://nhentai.net/g/242412/',
+              'https://nhentai.net/g/292478/','https://nhentai.net/g/205524/','https://nhentai.net/g/266448/','https://nhentai.net/g/89563/',
+              'https://nhentai.net/g/144725/','https://nhentai.net/g/191836/']
+    h = random.choice(hetero)
+    print (h)
+while 2 > 1:
+    print('qual vc vai querer agora?')
+    categoria = input()
+    if categoria == "furry":
+        furrys = [' https://nhentai.net/g/211170/ ', ' https://nhentai.net/g/210910/ ',
+                  ' https://nhentai.net/g/212653/ ', ' https://nhentai.net/g/212457/ ', 'https://nhentai.net/g/211136/',
+                  'https://nhentai.net/g/210590/', 'https://nhentai.net/g/210589/', 'https://nhentai.net/g/210275/',
+                  'https://nhentai.net/g/207520/', 'https://nhentai.net/g/207444/', 'https://nhentai.net/g/207303/',
+                  'https://nhentai.net/g/207271/', 'https://nhentai.net/g/207300/', 'https://nhentai.net/g/207151/',
+                  'https://nhentai.net/g/207125/', 'https://nhentai.net/g/207101/', 'https://nhentai.net/g/207012/',
+                  'https://nhentai.net/g/207011/', 'https://nhentai.net/g/207009/', 'https://nhentai.net/g/206970/',
+                  'https://nhentai.net/g/206967/', 'https://nhentai.net/g/206668/', 'https://nhentai.net/g/206569/',
+                  'https://nhentai.net/g/206381/', 'https://nhentai.net/g/206366/', 'https://nhentai.net/g/206301/',
+                  'https://nhentai.net/g/206271/', 'https://nhentai.net/g/138509/', 'https://nhentai.net/g/136224/']
+        f = random.choice(furrys)
+        print(f)
+    if categoria == "trap":
+        traps = ['https://nhentai.net/g/172937/', 'https://nhentai.net/g/259790/',
+                 'https://nhentai.net/g/150023/', 'https://nhentai.net/g/211129/', 'https://nhentai.net/g/312919/',
+                 'https://nhentai.net/g/283818/',
+                 'https://nhentai.net/g/236195/', 'https://nhentai.net/g/107296/', 'https://nhentai.net/g/164361/',
+                 'https://nhentai.net/g/263087/',
+                 'https://nhentai.net/g/208445/']
+        t = random.choice(traps)
+        print(t)
+    if categoria == "hetero":
+        hetero = ['https://nhentai.net/g/210910/18/', 'https://nhentai.net/g/243845/', 'https://nhentai.net/g/89379/',
+                  'https://nhentai.net/g/242412/',
+                  'https://nhentai.net/g/292478/', 'https://nhentai.net/g/205524/', 'https://nhentai.net/g/266448/',
+                  'https://nhentai.net/g/89563/',
+                  'https://nhentai.net/g/144725/', 'https://nhentai.net/g/191836/']
+        h = random.choice(hetero)
+        print(h)
